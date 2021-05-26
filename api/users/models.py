ceeefdc0bdea2c74e5025a0a18452b5672a3f616
Alejandro <jose.alejandro.her.ros@gmail.com>
2021-05-26 14:34:16 +0200

    Solved problems migrations users

--- api/users/models.py
+++ api/users/models.py
@@ -1,7 +1,24 @@
 from core.models import PublicIdModel, TimeStampedModel
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
-# Create your models here.
-class User(PublicIdModel, TimeStampedModel, AbstractBaseUser):
+class User(AbstractUser, PublicIdModel, TimeStampedModel):
     """Custom Model for users."""
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        unique=False,
+    )
+
+    email = models.EmailField(
+        _('email address'),
+        unique=True,
+        blank=True
+    )
